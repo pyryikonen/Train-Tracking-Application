6480bbecbdac263db86916dfa37c8a07d90769b9
Pyry Ikonen <pyrywilhelmikonen@gmail.com>
2024-04-04 12:48:32 +0300

    help function for fetching the trains in a separate file

--- app.py
+++ app.py
@@ -1,43 +1,24 @@
-import requests
-import json
 from flask import Flask, jsonify
 from flask_cors import CORS
 import threading
 import time
+import live_trains_fetcher  # Import the fetch_live_trains function from live_trains_fetcher.py
 
 app = Flask(__name__)
 CORS(app)  # Enable Cross-Origin Resource Sharing
 
-# Read station information from stations.json file
-with open('passenger_traffic_stations.json', 'r', encoding='utf-8') as file:
-    stations_data = json.load(file)
+# Route to get live trains for a specific station
+@app.route('/live-trains/<station_shortcode>')
+def get_live_trains(station_shortcode):
+    # Fetch live train data for the specified station
+    live_trains_data = live_trains_fetcher.fetch_live_trains(station_shortcode)
 
-# Extract station shortcodes
-major_stations = [station['stationShortCode'] for station in stations_data]
+    # Format the live train data
+    formatted_arriving_trains = format_live_trains_response(station_shortcode, live_trains_data['arriving'], 'arriving')
+    formatted_departing_trains = format_live_trains_response(station_shortcode, live_trains_data['departing'], 'departing')
 
-
-# Function to fetch live train information for a given station
-def fetch_live_trains(station_shortcode, minutes_before_departure=15, minutes_after_departure=15,
-                      minutes_before_arrival=15, minutes_after_arrival=15, arrived_trains=5, departing_trains=5):
-    url_arriving = f"https://rata.digitraffic.fi/api/v1/live-trains/station/{station_shortcode}?arriving_trains={arrived_trains}&minutes_before_arrival={minutes_before_arrival}&minutes_after_arrival={minutes_after_arrival}"
-    url_departing = f"https://rata.digitraffic.fi/api/v1/live-trains/station/{station_shortcode}?departing_trains={departing_trains}&minutes_before_departure={minutes_before_departure}&minutes_after_departure={minutes_after_departure}"
-
-    try:
-        response_arriving = requests.get(url_arriving)
-        response_departing = requests.get(url_departing)
-
-        response_arriving.raise_for_status()
-        response_departing.raise_for_status()
-
-        arriving_trains = response_arriving.json()
-        departing_trains = response_departing.json()
-
-        return {'arriving': arriving_trains, 'departing': departing_trains}
-
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching live trains for station {station_shortcode}: {e}")
-        return {'arriving': [], 'departing': []}
-
+    # Return the formatted data as JSON response
+    return jsonify({'arriving': formatted_arriving_trains, 'departing': formatted_departing_trains})
 
 # Function to format live train information response
 def format_live_trains_response(station_shortcode, live_trains, direction):
@@ -56,56 +37,31 @@ def format_live_trains_response(station_shortcode, live_trains, direction):
             'Difference in Minutes': None  # Add differenceInMinutes field
         }
 
-        for row in train['timeTableRows']:
-            formatted_row = {
-                'Station': row['stationShortCode'],
-                'Scheduled Time': row['scheduledTime']
-            }
-            formatted_train['Time Table Rows'].append(formatted_row)
+        # Get the first and last stations
+        first_station = train['timeTableRows'][0]
+        last_station = train['timeTableRows'][-1]
 
-            if row.get('actualTime') is not None:
-                formatted_train['Actual Time'] = row['actualTime']
-                formatted_train['Difference in Minutes'] = row['differenceInMinutes']
+        formatted_row_first = {
+            'Station': first_station['stationShortCode'],
+            'Scheduled Time': first_station['scheduledTime']
+        }
+
+        formatted_row_last = {
+            'Station': last_station['stationShortCode'],
+            'Scheduled Time': last_station['scheduledTime']
+        }
+
+        formatted_train['Time Table Rows'].append(formatted_row_first)
+        formatted_train['Time Table Rows'].append(formatted_row_last)
+
+        if last_station.get('actualTime') is not None:
+            formatted_train['Actual Time'] = last_station['actualTime']
+            formatted_train['Difference in Minutes'] = last_station['differenceInMinutes']
 
         formatted_trains.append(formatted_train)
 
     return formatted_trains
 
-
-
-live_trains_cache = {}
-
-
-# Function to fetch live trains periodically
-def fetch_live_trains_periodically():
-    global live_trains_cache
-    while True:
-        for station_shortcode in major_stations:
-            live_trains_cache[station_shortcode] = fetch_live_trains(station_shortcode)
-        time.sleep(900)  # 900 seconds = 15 minutes
-
-
-# Route to get live trains for a specific station
-@app.route('/live-trains/<station_shortcode>')
-def get_live_trains(station_shortcode):
-    global live_trains_cache
-    if station_shortcode in live_trains_cache:
-        arriving_trains = live_trains_cache[station_shortcode]['arriving']
-        departing_trains = live_trains_cache[station_shortcode]['departing']
-
-        formatted_arriving_trains = format_live_trains_response(station_shortcode, arriving_trains, 'arriving')
-        formatted_departing_trains = format_live_trains_response(station_shortcode, departing_trains, 'departing')
-
-        return jsonify({'arriving': formatted_arriving_trains, 'departing': formatted_departing_trains, })
-    else:
-        return jsonify({"error": "Station not found"}), 404
-
-
+# Run Flask app
 if __name__ == '__main__':
-    # Start a separate thread to fetch live trains periodically
-    fetch_thread = threading.Thread(target=fetch_live_trains_periodically)
-    fetch_thread.daemon = True
-    fetch_thread.start()
-
-    # Run Flask app
     app.run(debug=True, port=5001)  # You may want to set debug=False for production
---
+++ live_trains_fetcher.py
@@ -0,0 +1,59 @@
+import requests
+import json
+import time
+
+
+# Function to fetch live train information for a given station
+def fetch_live_trains(station_shortcode, minutes_before_departure=15, minutes_after_departure=15,
+                      minutes_before_arrival=15, minutes_after_arrival=15, arrived_trains=5, departing_trains=5):
+    url_arriving = f"https://rata.digitraffic.fi/api/v1/live-trains/station/{station_shortcode}?arriving_trains={arrived_trains}&minutes_before_arrival={minutes_before_arrival}&minutes_after_arrival={minutes_after_arrival}"
+    url_departing = f"https://rata.digitraffic.fi/api/v1/live-trains/station/{station_shortcode}?departing_trains={departing_trains}&minutes_before_departure={minutes_before_departure}&minutes_after_departure={minutes_after_departure}"
+
+    try:
+        response_arriving = requests.get(url_arriving)
+        response_departing = requests.get(url_departing)
+
+        response_arriving.raise_for_status()
+        response_departing.raise_for_status()
+
+        arriving_trains = response_arriving.json()
+        departing_trains = response_departing.json()
+
+        return {'arriving': arriving_trains, 'departing': departing_trains}
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching live trains for station {station_shortcode}: {e}")
+        return {'arriving': [], 'departing': []}
+
+
+# Function to fetch live train information periodically
+def fetch_live_trains_periodically():
+    while True:
+        for station_shortcode in major_stations:
+            try:
+                print("fetching trains to live cache")
+                live_trains_cache[station_shortcode] = fetch_live_trains(station_shortcode)
+            except requests.HTTPError as e:
+                if e.response.status_code == 429:  # Rate limit exceeded
+                    # Wait for an increasingly longer period before retrying
+                    sleep_duration = 2 ** (int(e.response.headers.get('Retry-After', 5)) + 1)
+                    print(f"Rate limit exceeded. Retrying in {sleep_duration} seconds.")
+                    time.sleep(sleep_duration)
+                else:
+                    print(f"Error fetching live trains for station {station_shortcode}: {e}")
+            time.sleep(1)  # Sleep between individual requests
+        time.sleep(10)  # Sleep between iterations
+
+
+if __name__ == "__main__":
+    # Read station information from stations.json file
+    with open('passenger_traffic_stations.json', 'r', encoding='utf-8') as file:
+        stations_data = json.load(file)
+
+    # Extract station shortcodes
+    major_stations = [station['stationShortCode'] for station in stations_data]
+
+    # Initialize live trains cache
+    live_trains_cache = {}
+
+    fetch_live_trains_periodically()
