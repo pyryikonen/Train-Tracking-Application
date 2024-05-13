2a5ce28ea55296a7943463e55edbc28a87d4420e
Pyry Ikonen <pyrywilhelmikonen@gmail.com>
2024-05-13 21:23:37 +0300

    Add: Different announcements for terminus stations

--- backend/utils/broadcast_utils.py
+++ backend/utils/broadcast_utils.py
@@ -39,21 +39,13 @@ def construct_arrival_broadcast(traindata_list):
     global arriving_audio_file_path
 
     for traindata in traindata_list:
-
         train_type = traindata.get('Train Type', 'Unknown')
-        print("Train Type:", train_type)
-
         if train_type is None:
             print("Train Type key not found in traindata")
             continue
 
         train_number = traindata.get('Train Number')
         time_table_rows = traindata.get('Time Table Rows', [])
-
-        # Print timeTableRows for debugging
-        print("TimeTableRows:")
-        for row in time_table_rows:
-            print(row)
 
         # Extract stations and scheduled times
         first_station = time_table_rows[0]['Station']
@@ -63,18 +55,25 @@ def construct_arrival_broadcast(traindata_list):
         target_station_name = get_station_name(target_station)
 
         scheduled_time_target = time_table_rows[1]['Scheduled Time']
+        track_number = time_table_rows[1].get('Track Number', 'Unknown')  # Extract track number
 
         time_24hr_target = f'{datetime.strptime(scheduled_time_target, "%Y-%m-%dT%H:%M:%S.%fZ").hour:02}:{datetime.strptime(scheduled_time_target, "%Y-%m-%dT%H:%M:%S.%fZ").minute:02}'
 
         train_number_as_text = str(train_number)
 
-        arriving_text = f'({train_type}-{train_number_as_text}) asemalta {first_station_name} pysähtyy asemalle {target_station_name} kello {time_24hr_target}.'
+        # Check if the target station is the same as the arrival station
+        if first_station == target_station:
+            # Construct a different announcement for final stations
+            arriving_text = f'(Juna {train_type}-{train_number_as_text}) saapuu asemalle {first_station_name}, raiteelle {track_number}. Tämä on junan {train_number_as_text} päätepysäkki.'
+        else:
+            arriving_text = f'(Juna {train_type}-{train_number_as_text}) asemalta {first_station_name} pysähtyy asemalle {target_station_name}, raiteelle {track_number} kello {time_24hr_target}.'
 
-        arriving_tts = gTTS(arriving_text, lang='fi', slow=True)
+        arriving_tts = gTTS(arriving_text, lang='fi')
         arriving_audio_file_path = f"static/departure_train_announcement_{train_number}.wav"
         arriving_tts.save(arriving_audio_file_path)
 
     return arriving_audio_file_path
+
 
 
 def construct_departure_broadcast(traindata_list):
@@ -107,15 +106,16 @@ def construct_departure_broadcast(traindata_list):
         last_station_name = get_station_name(last_station)
 
         scheduled_time_target = time_table_rows[1]['Scheduled Time']
+        track_number = time_table_rows[1].get('Track Number', 'Unknown')  # Extract track number
 
         time_24hr_target = f'{datetime.strptime(scheduled_time_target, "%Y-%m-%dT%H:%M:%S.%fZ").hour:02}:{datetime.strptime(scheduled_time_target, "%Y-%m-%dT%H:%M:%S.%fZ").minute:02}'
 
         train_number_as_text = str(train_number)
 
         # Departing train announcement
-        departing_text = f'({train_type}-{train_number_as_text}) asemalta {target_station_name} lähtee asemalle {last_station_name} kello {time_24hr_target}.'
+        departing_text = f'(Juna {train_type}-{train_number_as_text}) asemalta {target_station_name} raiteelta {track_number} lähtee asemalle {last_station_name} kello {time_24hr_target}.'
 
-        departing_tts = gTTS(departing_text, lang='fi', slow=True)
+        departing_tts = gTTS(departing_text, lang='fi')
         departing_audio_file_path = f"static/departure_train_announcement_{train_number}.wav"
         departing_tts.save(departing_audio_file_path)
 
